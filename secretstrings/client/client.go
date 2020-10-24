f6113a1f88ec87f5d2e75f372957c59347810eaf
rahahahat <rahat4uni@gmail.com>
2020-10-24 22:02:06 +0100

    adding client

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,16 +1,31 @@
 package main
+
 import (
-//	"net/rpc"
 	"flag"
-//	"bufio"
-//	"os"
-//	"secretstrings/stubs"
 	"fmt"
+	"io/ioutil"
+	"net/rpc"
+	"secretstrings/stubs"
+	"strings"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func main() {
+	data, err := ioutil.ReadFile("wordlist")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	sliceData := strings.Split(string(data), "\n")
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
-	//TODO: connect to the RPC server and send the request(s)
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+
+	for _, word := range sliceData {
+		request := stubs.Request{Message: string(word)}
+		response := new(stubs.Response)
+		client.Call(stubs.PremiumReverseHandler, request, response)
+		fmt.Println("Responded :" + response.Message)
+	} //TODO: connect to the RPC server and send the request(s)
 }
